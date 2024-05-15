b22cc194a53d000c687e6d3804fe5e74829f96aa
liutingli <liutingli@localhost>
2024-05-15 14:04:08 +0800

    Java

--- src/main/webapp/user/js/user.js
+++ src/main/webapp/user/js/user.js
@@ -20,7 +20,13 @@ const loadHTML = async (url, containerId) => {
 };
 
 // 渲染 User 資料配置
-const renderUser = () => ``;
+const renderUser = ({id,name,gender,age,birth,education,interestNames,interests,resume}) => `
+			<tr>
+				<td>${id}</td><td>${name}</td><td>${gender.name}</td><td>${age}</td>
+				<td>${birth}</td>
+				<td>${education.name}</td><td>${interestNames}</td><td>${resume}</td>
+				<td>修改</td><td>刪除</td>
+			</tr>`;
 
 // 資料渲染（資料所在地，目標位置，渲染方法）
 const fetchAndRenderData = async (url, containerId, renderFn) => {
@@ -29,6 +35,14 @@ const fetchAndRenderData = async (url, containerId, renderFn) => {
 		const response = await fetch(fullUrl); // 等待 fetch 請求完成
 		const {state, message, data} = await response.json(); // 等待回應本文內容
 		console.log(state, message, data);
+		// console.log(renderFn(data[0]));
+		// console.log(renderFn(data[1]));
+		// console.log(renderFn(data[2]));
+		// $(containerId).innerHTML = renderFn(data[0])+renderFn(data[1])+renderFn(data[2]);
+		// 三元運算元：如果data是陣列，就使用 map 多筆渲染，否則單筆
+		$(containerId).innerHTML = Array.isArray(data) ? data.map(renderFn).join('') : renderFn(data);
+		
+		
 	} catch(e) {
 		console.error(e);
 		$(containerId).innerHTML = '無法加載資料';
